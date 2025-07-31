de8597f79f51e63576bd120da3a15a566a6faa77
RevengeNMUS <nathanm5@ohs.stanford.edu>
2025-07-30 21:15:10 -0400

    Start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+//e
 @TeleOp(name = "gaming", group = "gamings")
 public class MainTeleOp extends OpMode {
     float yPower = 0;
